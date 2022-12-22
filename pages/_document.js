01e178817aa1910be4b441d53523fe3395a3cb52
Pedro Holguín <holguinpedro90@gmail.com>
2022-12-22 11:57:44 -0500

    add materialize

--- pages/_document.js
+++ pages/_document.js
@@ -3,10 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
       </body>
     </Html>
   )
